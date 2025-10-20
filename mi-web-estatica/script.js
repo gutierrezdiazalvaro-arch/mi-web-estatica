04f4443f983bbcc67454e41384cb388f098b3a19
AlvaroGutz <gutierrez.diaz.alvaro@iescamas.es>
2025-10-20 13:28:27 +0200

    Add files via upload

---
+++ mi-web-estatica/script.js
@@ -0,0 +1,18 @@
+// Esperamos a que el DOM cargue completamente antes de ejecutar el código
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById('toggleButton');
+    const extraInfo = document.getElementById('extraInfo');
+	const EraseWeb = document.getElementById('EraseWeb');
+
+    // Función para mostrar u ocultar la información adicional
+    toggleButton.addEventListener('click', function() {
+        if (extraInfo.style.display === "none") {
+            extraInfo.style.display = "block";
+            toggleButton.textContent = "Ocultar información";
+        } else {
+            extraInfo.style.display = "none";
+            toggleButton.textContent = "Mostrar más información";
+		
+        }
+    });
+});
